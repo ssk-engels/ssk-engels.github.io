46468ec79f50ba3f9dda43e26c26a674e0086518
ousenko <fff@email.com>
2014-01-10 16:34:06 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+##README
+
+This site was constructed for SSK organisation of Engels city.
+Blog is under control of private person.
---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+jQuery(function($) {
+  $('.scroll').click(function() {
+    var anchor_name = $(this).attr('href').split('#')[1];
+    var anchor = $('#' + anchor_name);
+    $('html, body').animate({
+      scrollTop: anchor.offset().top
+    }, 600);
+    return false;
+  });
